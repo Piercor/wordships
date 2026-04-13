0b17830601fe304fb6da54b8b3db9b159154d254
theazukanovic <thea.zukanovic@student.nbi-handelsakademin.se>
2026-04-13 12:11:09 +0200

    Add createEmptyGrid function.

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,6 +5,15 @@ interface Square {
   hasWord: boolean;
 }
 
+function createEmptyGrid(): Square[][] {
+  return Array.from({ length: 10 }, () =>  // skapa 10 rader
+    Array.from({ length: 10 }, () => ({    // skapa 10 celler per rad
+      letter: null,
+      revealed: false,
+      hasWord: false,
+    }))
+  );
+}
 
 export default function Grid() {
   return (
