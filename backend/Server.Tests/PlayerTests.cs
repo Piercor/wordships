57440c04d739a3fc5069ee322fe02d2619e59417
Piercor <pierinocr@gmail.com>
2026-04-13 20:50:17 +0200

    add/ more xUnit tests

--- backend/Server.Tests/PlayerTests.cs
+++ backend/Server.Tests/PlayerTests.cs
@@ -2,7 +2,7 @@ namespace Server.Tests;
 
 public class PlayerTests
 {
-  private readonly Player _player = new(Guid.NewGuid(), "player1", Game.GetWords());
+  private readonly Player _player = new(Guid.NewGuid(), "Player1", Game.GetWords());
 
   [Fact]
   public void GetId_Test()
@@ -17,9 +17,26 @@ public class PlayerTests
   }
 
   [Fact]
-  public void GetWordList_Test()
+  public void PlayerWordList_Test()
   {
-    Assert.NotNull(_player.WordList);
+    Assert.NotNull(_player.GetWordList());
+    Assert.Equal(10, _player.WordList.Count);
+  }
+
+  [Theory]
+  [InlineData(0, 3)]
+  [InlineData(1, 3)]
+  [InlineData(2, 3)]
+  [InlineData(3, 3)]
+  [InlineData(4, 4)]
+  [InlineData(5, 4)]
+  [InlineData(6, 4)]
+  [InlineData(7, 5)]
+  [InlineData(8, 5)]
+  [InlineData(9, 6)]
+  public void PlayerWordListLength_Test(int index, int expected)
+  {
+    Assert.Equal(_player.WordList[index].LetterList.Count, expected);
   }
 
 }
