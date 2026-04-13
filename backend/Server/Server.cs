3e1897dd53089035c431a06a34b756ffcc50c29f
Piercor <pierinocr@gmail.com>
2026-04-13 22:14:32 +0200

    removed old endpoint and added new ones

--- backend/Server/Server.cs
+++ backend/Server/Server.cs
@@ -1,6 +1,9 @@
+using Server;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/api/hello", () => new { message = "Hello from .NET!" });
+
+app.GameEndpoints();
 
 app.Run();
