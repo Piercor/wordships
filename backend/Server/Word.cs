7633e404e23728d9952f88ecd9018505f70a3593
Piercor <pierinocr@gmail.com>
2026-04-14 22:02:55 +0200

    Work in progress

--- backend/Server/Word.cs
+++ backend/Server/Word.cs
@@ -3,4 +3,7 @@ namespace Server;
 public record Word(string Name)
 {
   public List<char> LetterList => Name.ToList();
+  /*   public List<Letter> LetterList => Name
+    .Select(c => new Letter { Value = c })
+    .ToList(); */
 }
