55f4e7c1415791259b31c666d2c6f31ba7dbf3e8
Piercor <pierinocr@gmail.com>
2026-04-14 16:04:03 +0200

    add/ method to get player by Id

--- backend/Server/GameEngine.cs
+++ backend/Server/GameEngine.cs
@@ -3,4 +3,20 @@ namespace Server;
 public static class GameEngine
 {
   public static Dictionary<Guid, Game> Games { get; } = new();
+
+  public static Player? GetPlayer(Guid PlayerId)
+  {
+    foreach ((Guid key, Game game) in Games)
+    {
+      if (game.Player1.Id == PlayerId)
+      {
+        return game.Player1;
+      }
+      else if (game.Player2.Id == PlayerId)
+      {
+        return game.Player2;
+      }
+    }
+    return null;
+  }
 }
