3f8d6877793d770b1eac6efe3e82cff3509061c6
Piercor <pierinocr@gmail.com>
2026-04-14 20:52:02 +0200

    fix/ Player 1 & 2 no part of constructor anymore

--- backend/Server/Game.cs
+++ backend/Server/Game.cs
@@ -3,14 +3,12 @@ namespace Server;
 public class Game
 {
   public Guid Id;
-  public Player Player1;
-  public Player Player2;
+  public Player? Player1;
+  public Player? Player2;
 
-  public Game(Guid id, Player player1, Player player2)
+  public Game(Guid id)
   {
     Id = id;
-    Player1 = player1;
-    Player2 = player2;
   }
   public Guid GetId()
   {
