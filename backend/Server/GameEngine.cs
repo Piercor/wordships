a82265bb812741d795f079d69e4cf0957ea10e78
Piercor <pierinocr@gmail.com>
2026-04-14 22:02:03 +0200

    Work in progress

--- backend/Server/GameEngine.cs
+++ backend/Server/GameEngine.cs
@@ -4,19 +4,32 @@ public static class GameEngine
 {
   public static Dictionary<Guid, Game> Games { get; } = new();
 
-  public static Player? GetPlayer(Guid PlayerId)
+  public static Player? GetPlayer(Guid playerId)
   {
     foreach ((Guid key, Game game) in Games)
     {
-      if (game.Player1.Id == PlayerId)
+      if (game?.Player1?.Id == playerId)
       {
         return game.Player1;
       }
-      else if (game.Player2.Id == PlayerId)
+      else if (game?.Player2?.Id == playerId)
       {
         return game.Player2;
       }
     }
     return null;
   }
+
+  /* public static bool PlayerHasLetter(Guid gameId, Guid playerId, char letter)
+  {
+    var player = GameEngine.GetPlayer(playerId);
+
+    foreach (Word word in player.WordList)
+    {
+      if (word.LetterList.Contains(letter))
+      {
+        
+      }
+    }
+  } */
 }
