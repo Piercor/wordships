95b98f5f250485d85660ddc5445ca19bd6a7755b
malinkytta <malinkytta@gmail.com>
2026-04-16 17:53:30 +0200

    Add placement and waiting pages to app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import RegisterPlayerPage from "./pages/RegisterPlayerPage";
 import { GameProvider, useGame } from "./context/GameContext";
 import CreateOrJoinPage from "./pages/CreateOrJoinPage";
+import WaitingPage from "./pages/WaitingPage";
+import PlacementPage from "./pages/PlacementPage";
 
 const AppContent = () => {
-  const { player, gameId } = useGame();
+  const { player, gameId, isReady, bothReady } = useGame();
 
   if (!player) {
     return <RegisterPlayerPage />;
   }
 
-  if (!gameId) {
-    return <CreateOrJoinPage />;
-  }
+  if (!gameId) return <CreateOrJoinPage />;
+
+  if (!isReady) return <PlacementPage />;
+  if (!bothReady) return <WaitingPage />;
 
   //TODO: ska returnera sidan för själva spelet sen
-  return <div>Väntar på motspelare... (Game ID: {gameId})</div>;
+  return <div>Game</div>;
 };
 
 export default function App() {
