b04c2008b36f9d663f7dd705dd0e1a003d8c41f3
malinkytta <malinkytta@gmail.com>
2026-04-19 11:27:48 +0200

    Update interfaces for Grid and Player

--- src/interface/Grid.ts
+++ src/interface/Grid.ts
@@ -1,8 +1,7 @@
-import type { Player } from "./Player";
-
-export type Square = (string | null)[][];
-export interface GridProps {
-  opponent: boolean;
-  grid: Square;
-  player: Player | null;
+export interface Cell {
+  letter: string | null;
+  wordName: string | null;
+  found: boolean;
 }
+
+export type Square = Cell[][];
