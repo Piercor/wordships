67d53b134bc5e0d3bfd8e5e7096fbae3252c18ce
malinkytta <malinkytta@gmail.com>
2026-04-19 11:30:07 +0200

    Replace empty cells with typed Cell objects in createEmptyGrid

--- src/utils/createEmptyGrid.ts
+++ src/utils/createEmptyGrid.ts
@@ -1,7 +1,11 @@
-import type { Square } from "../interface/Grid";
+import type { Square, Cell } from "../interface/Grid";
 
 export function createEmptyGrid(): Square {
   return Array.from({ length: 10 }, () =>
-    Array.from({ length: 10 }, () => null),
+    Array.from({ length: 10 }, (): Cell => ({
+      letter: null,
+      wordName: null,
+      found: false
+    })),
   );
 }
