d928f3cf2dd600250cb25d36491bf95bd71e87bb
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2026-04-21 15:00:11 +0200

    added reference for .csv file

--- backend/Server/Game.cs
+++ backend/Server/Game.cs
@@ -2,101 +2,105 @@ namespace Server;
 
 public class Game
 {
-  public Guid Id;
-  public Player? Player1;
-  public Player? Player2;
-  public Player? Turn;
-  public Player? Winner = null;
-  public Game(Guid id)
-  {
-    Id = id;
-  }
-  public Guid GetId()
-  {
-    return Id;
-  }
-  public Player? GetPlayer(Guid playerId)
-  {
-    if (Player1?.Id == playerId)
-    {
-      return Player1;
-    }
-    else if (Player2?.Id == playerId)
-    {
-      return Player2;
-    }
-    else
-    {
-      return null;
-    }
-  }
-  public static List<Word> GetWords()
-  {
-    Dictionary<int, List<string>> words = new();
-    string[] wordsCsv = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "Server", "src", "words.csv"));
+    public Guid Id;
+    public Player? Player1;
+    public Player? Player2;
+    public Player? Turn;
+    public Player? Winner = null;
 
-    foreach (string line in wordsCsv)
+    public Game(Guid id)
     {
-      string[] wordsSplitData = line.Split(",");
+        Id = id;
+    }
 
-      foreach (string word in wordsSplitData)
-      {
-        int length = word.Length;
-        if (!words.ContainsKey(length))
+    public Guid GetId()
+    {
+        return Id;
+    }
+
+    public Player? GetPlayer(Guid playerId)
+    {
+        if (Player1?.Id == playerId)
         {
-          words[length] = new List<string>();
+            return Player1;
         }
-        words[length].Add(word);
-      }
+        else if (Player2?.Id == playerId)
+        {
+            return Player2;
+        }
+        else
+        {
+            return null;
+        }
     }
 
-    List<Word> randomWords = new();
-
-    foreach ((int key, List<string> wordList) in words)
+    public static List<Word> GetWords()
     {
-      Random rnd = new Random();
-      switch (key)
-      {
-        case 3:
-          for (int i = 0; i < 4; i++)
-          {
-            int index = rnd.Next(0, wordList.Count);
-            randomWords.Add(new(wordList[index]));
-          }
-          break;
-        case 4:
-          for (int i = 0; i < 3; i++)
-          {
-            int index = rnd.Next(0, wordList.Count);
-            randomWords.Add(new(wordList[index]));
-          }
-          break;
-        case 5:
-          for (int i = 0; i < 2; i++)
-          {
-            int index = rnd.Next(0, wordList.Count);
-            randomWords.Add(new(wordList[index]));
-          }
-          break;
-        case 6:
-          {
-            int index = rnd.Next(0, wordList.Count);
-            randomWords.Add(new(wordList[index]));
-          }
-          break;
-      }
-    }
-    randomWords.Reverse();
-    return randomWords;
-  }
-  public static Player CreatePlayer(string name)
-  {
-    return new(Guid.NewGuid(), name, GetWords());
-  }
+        Dictionary<int, List<string>> words = new();
+        string[] wordsCsv = File.ReadAllLines(Path.Combine(AppContext.BaseDirectory, "words.csv"));
+        foreach (string line in wordsCsv)
+        {
+            string[] wordsSplitData = line.Split(",");
 
-  public Player? FirstTurn()
-  {
-    Random rnd = new Random();
-    return rnd.Next(2) == 0 ? Player1 : Player2;
-  }
+            foreach (string word in wordsSplitData)
+            {
+                int length = word.Length;
+                if (!words.ContainsKey(length))
+                {
+                    words[length] = new List<string>();
+                }
+                words[length].Add(word);
+            }
+        }
+
+        List<Word> randomWords = new();
+
+        foreach ((int key, List<string> wordList) in words)
+        {
+            Random rnd = new Random();
+            switch (key)
+            {
+                case 3:
+                    for (int i = 0; i < 4; i++)
+                    {
+                        int index = rnd.Next(0, wordList.Count);
+                        randomWords.Add(new(wordList[index]));
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < 3; i++)
+                    {
+                        int index = rnd.Next(0, wordList.Count);
+                        randomWords.Add(new(wordList[index]));
+                    }
+                    break;
+                case 5:
+                    for (int i = 0; i < 2; i++)
+                    {
+                        int index = rnd.Next(0, wordList.Count);
+                        randomWords.Add(new(wordList[index]));
+                    }
+                    break;
+                case 6:
+                    {
+                        int index = rnd.Next(0, wordList.Count);
+                        randomWords.Add(new(wordList[index]));
+                    }
+                    break;
+            }
+        }
+        randomWords.Reverse();
+        return randomWords;
+    }
+
+    public static Player CreatePlayer(string name)
+    {
+        return new(Guid.NewGuid(), name, GetWords());
+    }
+
+    public Player? FirstTurn()
+    {
+        Random rnd = new Random();
+        return rnd.Next(2) == 0 ? Player1 : Player2;
+    }
 }
