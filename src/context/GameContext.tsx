6da948ac4cfc462505c5d19e7c4e080535f4202e
malinkytta <malinkytta@gmail.com>
2026-04-21 11:21:28 +0200

    Update body in guessLetter function in gamecontext to match new backend

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -142,7 +142,12 @@ export const GameProvider = ({ children }: { children: React.ReactNode }) => {
     const result = await fetch("/api/player/guess", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ gameId, playerId: opponent?.id, letter }),
+      body: JSON.stringify({
+        gameId,
+        playerGuessingId: player?.id,
+        playerToGuessId: opponent?.id,
+        letter,
+      }),
     });
 
     if (!result.ok) throw new Error("Failed to guess letter");
