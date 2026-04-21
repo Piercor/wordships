0d7a8a7427de2269eabe14a527b1edaf9d11a3dd
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2026-04-21 10:58:06 +0200

    Added endpoint api/health

--- backend/Server/Server.cs
+++ backend/Server/Server.cs
@@ -3,8 +3,9 @@ using Server;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
 app.GameEndpoints();
 app.PlayerEndpoints();
+
+app.MapGet("/api/health", () => Results.Ok("OK"));
 
 app.Run();
